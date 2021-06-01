e3c7b724de85235d33325a2847aca90c9fc62755
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-01 13:39:57 +0200

    Add custom validation

--- src/app/demo-form-with-custom-validation/demo-form-with-custom-validation.ts
+++ src/app/demo-form-with-custom-validation/demo-form-with-custom-validation.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
+function skuValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return { invalidSku: true };
+  }
+  return {};
+}
+ 
 @Component({
   selector: 'app-demo-form-with-custom-validation',
   templateUrl: './demo-form-with-custom-validation.html',
@@ -11,17 +24,17 @@ export class DemoFormWithCustomValidationComponent implements OnInit {
 
   constructor(fb: FormBuilder) {
     this.myForm = fb.group({
-      'sku': ['', Validators.required] 
+      sku: ['', Validators.compose([
+        Validators.required, skuValidator
+      ])],
     });
 
     this.sku = this.myForm.controls['sku'];
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit(value: string): void {
     console.log('Submitted:', value);
   }
-
 }
